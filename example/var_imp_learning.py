3655faeabde30416e21cc3d88bf068ad2a5d333e
Jeremiah Zhe Liu <zhel90@gmail.com>
2019-08-08 16:17:22 -0400

    add experiment to learn variable importance

--- example/var_imp_learning.py
+++ example/var_imp_learning.py
@@ -29,35 +29,26 @@ WEIGHT_PRIOR_SD = np.sqrt(.1).astype(dtype_util.NP_DTYPE)
 # if __name__ == "__main__":
 logdir = "./tmp/"
 
-config = {"n_train": 1000, "n_feature": 100,
-          "n_node": 50, "n_layer": 2,
-          "hidden_weight_sd": np.sqrt(.1).astype(dtype_util.NP_DTYPE),
-          "output_weight_sd": .1}
-
-n_train = 100
-n_feature = 50
-n_feature_true = 5
-
-num_sample = int(5e3)
-num_burnin = int(1e4)
-num_pred_sample = 250
+data_config = {"n": 500, "d": 50, "d_true": 5,
+               "data_gen_func": data_util.data_gen_func_linear,
+               "random_seed_f": None}
 
 model_config = {"n_node": 50, "n_layer": 2,
                 "hidden_weight_sd": WEIGHT_PRIOR_SD,
                 "output_weight_sd": .1}
 
+mcmc_config = {"num_sample": int(5e3),
+               "num_burnin": int(1e4),
+               "num_pred_sample": 250}
+
 # generate training data
-(y_train, X_train, f_test, X_test,
- true_var_imp) = data_util.generate_data(n=n_train,
-                                         d=n_feature,
-                                         d_true=n_feature_true,
-                                         data_gen_func=data_util.data_gen_func_linear,
-                                         random_seed_f=50)
+(y_train, X_train,
+ f_test, X_test, true_var_imp) = data_util.generate_data(**data_config)
 
 # 1. Build Model and Inference Graph ################
 (param_samples, is_accepted, param_names,
  model_fn, mcmc_graph) = exp_util.make_bnn_graph(X_train, y_train,
-                                                 num_sample, num_burnin,
+                                                 **mcmc_config,
                                                  **model_config)
 
 # 2. Execute Training then Predict ################
@@ -65,7 +56,7 @@ param_sample_dict = mcmc.sample_parameter(param_samples, is_accepted,
                                           param_names, mcmc_graph)
 
 (pred_sample, imp_sample,
- bias_samples) = mcmc.sample_predictive(num_pred_sample,
+ bias_samples) = mcmc.sample_predictive(mcmc_config["num_pred_sample"],
                                         param_sample_dict,
                                         model_fn, X_test)
 
@@ -77,5 +68,5 @@ print("predict MSE:{:4f}".format(np.mean((pred_mean - f_test) ** 2)))
 print("var_imp MSE:{:4f}".format(np.mean((var_imp_mean - true_var_imp) ** 2)))
 
 # optionally, visualize
-visual_util.plot_var_imp(imp_sample, f_test, n_variable=50)
+visual_util.plot_var_imp(imp_sample, true_var_imp, n_variable=50)
 visual_util.plot_prediction(pred_sample, f_test)
